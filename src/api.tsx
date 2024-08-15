b1b92a7e6223ac01ec8b6c2cf87347bcd8668748
Maksim Zakharov <max89701@gmail.com>
2024-08-15 20:58:24 +0300

    build ci

--- src/api.tsx
+++ src/api.tsx
@@ -3,9 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
     reducerPath: 'api',
     tagTypes: [],
-    baseQuery: fetchBaseQuery({
-        baseUrl: process.env.BASE_URL
-    }),
+    baseQuery: fetchBaseQuery(),
     endpoints: (builder) => ({
         getCars: builder.query<any, any>({
             query: (params) => ({ method: 'GET', url: '/cars', params })
