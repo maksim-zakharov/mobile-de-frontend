d5f76f791c611117b0b5bfac1ca0bc9a514c560d
Maksim Zakharov <max89701@gmail.com>
2024-08-20 21:00:48 +0300

    Комплектация

--- src/pages/CarDetailsPage/CarDetailsPage.tsx
+++ src/pages/CarDetailsPage/CarDetailsPage.tsx
@@ -10,7 +10,7 @@ const CarDetailsPage = () => {
     const {id} = useParams();
     const [searchParams, setSearchParams] = useSearchParams();
     let userId = searchParams.get('userId');
-    if(window.Telegram?.WebApp?.initDataUnsafe){
+    if (window.Telegram?.WebApp?.initDataUnsafe) {
         userId = window.Telegram.WebApp.initDataUnsafe.user?.id;
     }
 
@@ -19,11 +19,14 @@ const CarDetailsPage = () => {
     });
 
     const technical = data?.attributes || [];
+    const features = data?.features || [];
 
     return <div className="container CarDetailsPage">
         <div className="PageContainer">
             <Button icon={<LeftOutlined/>} style={{margin: '16px 0 0'}} type="link" size="large"
                     onClick={() => window.history.back()}/>
+            {isFetching && <Spin style={{width: '100%', height: '100%'}}/>}
+            {isError && <Button onClick={refetch} style={{margin: 'auto'}} type="primary">Обновить</Button>}
             <h1 className="PageCard__title">{data?.title}</h1>
             <div className="CardPriceNew-">
                 <span className="CardPriceNew__price">{moneyFormat(data?.price, 0, 0)}</span>
@@ -31,15 +34,17 @@ const CarDetailsPage = () => {
             </div>
             <Carousel className="OfferGallery">
                 {data?.imgUrls.map(img => <img src={img.replace('mo-160', 'mo-360')}
-                                        style={{width: '360px'}}
-                                        alt=""/>)}
+                                               style={{width: '360px'}}
+                                               alt=""/>)}
             </Carousel>
             <h2>Характеристики</h2>
-            {isFetching && <Spin style={{width: '100%', height: '100%'}}/>}
-            {isError && <Button onClick={refetch} style={{margin: 'auto'}} type="primary">Обновить</Button>}
             {technical.map(item => <div className="labelValue" key={item.key}>
                 <div className="label">{item.label}</div>
                 <div className="value">{item.value}</div>
+            </div>)}
+            <h2>Комплектация</h2>
+            {features.map(item => <div className="features__name"><span
+                className="catalog-option__name">{item}</span>
             </div>)}
         </div>
     </div>
